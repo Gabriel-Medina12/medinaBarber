94c0899c144361efa682e59bdcae43f8aaae1501
Gabriel Medina <saulymarclash12@gmail.com>
2025-04-04 12:13:33 -1200

    cambios x

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -366,16 +366,5 @@ router.post('/reset-password/:token', async (req, res) => {
   }
 });
 
-router.post('/logout', async(req, res)=>{
-
-    try {
-        // En un sistema más avanzado, podrías agregar el token a una lista negra.
-        res.status(200).json({ message: 'Sesión cerrada exitosamente' });
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Error en el servidor' });
-      }
-
-});
 
 module.exports = router;
