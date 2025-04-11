8a9856798881ec99ba61d791e439f5c764485446
Gabriel Medina <saulymarclash12@gmail.com>
2025-04-11 02:11:28 -0400

    Cambios en header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import Loader from "./components/Loader.jsx";
-import Header from "./components/header.jsx";
+import Header from "./components/Header.jsx";
 import Auth from "./pages/auth/login.jsx";
 import Home from "./pages/home.jsx";
 import ForgotPassword from "./pages/auth/forgotPassword.jsx";
